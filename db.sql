14e6197407cfa5af0e720503475c5a39de8d3d67
Bernardo Bicalho <91989612+alvexx45@users.noreply.github.com>
2024-08-22 09:40:23 -0300

    Add files via upload

---
+++ db.sql
@@ -0,0 +1,24 @@
+-- Database: exercicio02
+
+-- DROP DATABASE IF EXISTS exercicio02;
+
+-- CREATE DATABASE exercicio02
+--     WITH
+--     OWNER = ti2cc
+--     ENCODING = 'UTF8'
+--     LC_COLLATE = 'en_US.UTF-8'
+--     LC_CTYPE = 'en_US.UTF-8'
+--     LOCALE_PROVIDER = 'libc'
+--     TABLESPACE = pg_default
+--     CONNECTION LIMIT = -1
+--     IS_TEMPLATE = False;
+
+-- CREATE TABLE usuario (
+-- 	codigo SERIAL PRIMARY KEY,
+-- 	nome TEXT NOT NULL,
+-- 	senha TEXT NOT NULL,
+-- 	sexo CHAR(1) NOT NULL
+-- )
+
+
+-- SELECT * FROM usuario;
